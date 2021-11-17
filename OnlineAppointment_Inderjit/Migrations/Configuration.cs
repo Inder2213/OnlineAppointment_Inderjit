865e99a1ba7b8f69fb75a196d8925ad9c3333000
indersidhu362@gmail.com <indersidhu362@gmail.com>
2021-11-17 21:54:42 +0300

    Add project files.

---
+++ OnlineAppointment_Inderjit/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace OnlineAppointment_Inderjit.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OnlineAppointment_Inderjit.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OnlineAppointment_Inderjit.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
